3bf832867e68391295f807c643999ea4f5edce8f
matruskan <matruskan@gmail.com>
2018-06-10 16:46:17 -0300

    Fetch coin prices from cache, or from min-api.cryptocompare.com webservice

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,6 +1,23 @@
 class CoinsController < ApplicationController
     def index
         @coins = Coin.all
+        @coins.each do |coin|
+            cache = ActiveSupport::Cache::MemoryStore.new(expires_in: 10.minute)
+            price = cache.fetch(coin.symbol) do
+                response = RestClient.get 'https://min-api.cryptocompare.com/data/price', {params: {'fsym' => coin.symbol, 'tsyms' => 'USD'}}
+                JSON.parse(response)['USD']
+            end
+            coin.price = price
+        end
+    end
+    def show
+        @coin = Coin.find(params[:id])
+        cache = ActiveSupport::Cache::MemoryStore.new(expires_in: 10.minute)
+        price = cache.fetch(@coin.symbol) do
+            response = RestClient.get 'https://min-api.cryptocompare.com/data/price', {params: {'fsym' => @coin.symbol, 'tsyms' => 'USD'}}
+            JSON.parse(response)['USD']
+        end
+        @coin.price = price
     end
     def new
     end
