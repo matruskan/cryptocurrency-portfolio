907e2ff26cfc780f80196b11a3630def22822244
matruskan <matruskan@gmail.com>
2018-06-12 20:29:54 -0300

    Add 'create' method in 'purchase_orders_controller' for form submition

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -9,4 +9,15 @@ class PurchaseOrdersController < ApplicationController
         @purchaseOrder.coin.price = CoinsController.fetch_price(@purchaseOrder.coin.symbol)
         @coin = @purchaseOrder.coin
     end
+
+    def create
+        #render plain: params[:purchaseOrder].inspect
+        @purchaseOrder = PurchaseOrder.new(params.require(:purchaseOrder).permit(:quantity))
+        coin = Coin.new(params.require(:coin).permit(:id))
+        @purchaseOrder.coin = Coin.find(coin.id)
+        @purchaseOrder.coin.price = CoinsController.fetch_price(@purchaseOrder.coin.symbol)
+        @purchaseOrder.pricePaid = @purchaseOrder.coin.price * @purchaseOrder.quantity
+        @purchaseOrder.save
+        redirect_to @purchaseOrder
+    end
 end
