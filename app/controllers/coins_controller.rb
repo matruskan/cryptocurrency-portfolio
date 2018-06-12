258beada0ebfdc7056994e12791cdb9d61ba223a
matruskan <matruskan@gmail.com>
2018-06-12 20:01:50 -0300

    Move common code to 'fetch_price' method

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,24 +1,25 @@
 class CoinsController < ApplicationController
+
+    def self.fetch_price(symbol)
+        cache = ActiveSupport::Cache::MemoryStore.new(expires_in: 10.minute)
+        cache.fetch(symbol) do
+            response = RestClient.get 'https://min-api.cryptocompare.com/data/price', {params: {'fsym' => symbol, 'tsyms' => 'USD'}}
+            JSON.parse(response)['USD']
+        end
+    end
+
     def index
         @coins = Coin.all
         @coins.each do |coin|
-            cache = ActiveSupport::Cache::MemoryStore.new(expires_in: 10.minute)
-            price = cache.fetch(coin.symbol) do
-                response = RestClient.get 'https://min-api.cryptocompare.com/data/price', {params: {'fsym' => coin.symbol, 'tsyms' => 'USD'}}
-                JSON.parse(response)['USD']
-            end
-            coin.price = price
+            coin.price = CoinsController.fetch_price(coin.symbol)
         end
     end
+
     def show
         @coin = Coin.find(params[:id])
-        cache = ActiveSupport::Cache::MemoryStore.new(expires_in: 10.minute)
-        price = cache.fetch(@coin.symbol) do
-            response = RestClient.get 'https://min-api.cryptocompare.com/data/price', {params: {'fsym' => @coin.symbol, 'tsyms' => 'USD'}}
-            JSON.parse(response)['USD']
-        end
-        @coin.price = price
+        @coin.price = CoinsController.fetch_price(@coin.symbol)
     end
+
     def new
     end
 end
