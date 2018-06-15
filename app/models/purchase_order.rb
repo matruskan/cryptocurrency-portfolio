41b12e3bfed36a3e67a9d6a421aaa78acf23cdde
matruskan <matruskan@gmail.com>
2018-06-15 01:55:04 -0300

    Add validation to Purchase Order pricePaid and quantity

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -1,4 +1,8 @@
 class PurchaseOrder < ApplicationRecord
   belongs_to :coin
   attr_accessor :priceNow
+  validates :quantity, presence: true,
+                       numericality: {greater_than: 0}
+  validates :pricePaid, presence: true,
+                        numericality: {greater_than: 0}
 end
